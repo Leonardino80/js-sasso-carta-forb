40e3531c7f5258eeb4cb2cf3e1067776f62011c5
Leonardino80 <gilgamesh2005@gmail.com>
2019-05-02 05:10:37 +0200

    introduzione funzione

--- main.js
+++ main.js
@@ -3,3 +3,8 @@
 var scelta = [ 'carta', 'forbice', 'sasso'];
 var scelta_PL1;
 var scelta_PL2;
+//  introduco una funzione per la scelta
+alert('inizia la partita!');
+function carta_forbice_sasso ( scelta_casuale ) {
+  var scelta_casuale = scelta[Math.floor(Math.random() * scelta.length)];
+return scelta_casuale}
